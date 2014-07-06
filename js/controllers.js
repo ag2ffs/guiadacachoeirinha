b143ee90caf88d4b9ba5b8e65e9ddd8b652f534c
Jeff <jeff@webgoal.com.br>
2014-07-06 15:16:15 -0300

    Nova marca

--- js/controllers.js
+++ js/controllers.js
@@ -16,11 +16,10 @@ adscatApp.controller('AdsListCtrl', function($scope , $timeout) {
 	});
 	
 	$scope.ads = [
-		{'name': 'Webgoal', 'description': 'Produtos Web Software', 'image': 'webgoal.jpg'},
-		{'name': 'Roda de Bikes', 'description': 'Bicicletas motorizadas', 'image': 'roda-bike.jpg'},
+		{'name': 'Fruta na Caixa', 'description': 'Frutas Fresquinhas todos os dias na sua casa', 'image': 'fruta-na-caixa.jpg', 'endereco': 'Rua 22 de agosto, 142'},
+		{'name': 'Van Bikes', 'description': 'Bicicletas motorizadas', 'image': 'roda-bike.jpg'},
 		{'name': 'Carlinhos Cabeleleiros', 'description': 'Cortes Masculinos', 'image': 'carlinhos.jpg'},
-		{'name': 'Bolos da Lurdes', 'description': 'Bolos para festas', 'image': 'bolo-lourdes.jpg'},
-		{'name': 'Fruta na Caixa', 'description': 'Frutas Fresquinhas todos os dias na sua casa', 'image': 'fruta-na-caixa.jpg'},
+		{'name': 'Bolos da Lourdes', 'description': 'Bolos para festas', 'image': 'bolo-lourdes.jpg'},
 		{'name': 'Balão Mágico', 'description': 'Buffet para festas infantis', 'image': 'balloon.jpg'},
 		{'name': 'Bar do Ze', 'description': 'Almoço e Lanches', 'image': 'bar-do-ze.jpg'},
 		{'name': 'Sapataria do futuro', 'description': 'Sapatos sempre novos', 'image': 'sapataria-do-futuro.jpg'},
